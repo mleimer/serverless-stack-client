243581bbd65e8ac68572bb4aa623bda07ca1ba6c
Martin Leimer <martin.leimer@zuehlke.com>
2020-08-01 12:16:21 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "smartin-tutorial"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2gbrc1xl99.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_HzKaf0OIg",
+    APP_CLIENT_ID: "2k3bh9q78s6oejhk38jdgj2ecm",
+    IDENTITY_POOL_ID: "us-east-1:2dc8adae-de46-4b4f-9f09-b4bcbb2c5657"
+  }
+};
